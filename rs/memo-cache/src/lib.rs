19c6ab92e37112d3fd574c777774fcb2ce293043
Kris van Rens <krisvanrens@gmail.com>
2024-08-27 20:30:24 +0200

    RFT: Splitup internal/external tests.

--- rs/memo-cache/src/lib.rs
+++ rs/memo-cache/src/lib.rs
@@ -126,7 +126,7 @@ where
 }
 
 #[cfg(test)]
-mod tests {
+mod tests_internal {
     use super::*;
 
     #[test]
@@ -140,15 +140,6 @@ mod tests {
 
         // All slots should be empty.
         assert!(c.buffer.iter().all(|s| s == &KeyValueSlot::Empty));
-    }
-
-    #[test]
-    fn test_empty() {
-        let c = MemoCache::<bool, bool, 2>::new();
-
-        // NOTE: Even though the cache memory is pre-allocated, each cache slot should be marked as "empty".
-        assert!(c.find(true).is_none());
-        assert!(c.find(false).is_none());
     }
 
     #[test]
@@ -172,79 +163,5 @@ mod tests {
         c.insert(7, 8);
 
         assert_eq!(c.cursor, 0);
-    }
-
-    #[test]
-    fn test_nonempty() {
-        let mut c = MemoCache::<String, i32, 3>::new();
-
-        let kvs = vec![("veni", 19), ("vidi", 23), ("vici", 29)];
-        let kv0 = kvs.get(0).unwrap();
-        let kv1 = kvs.get(1).unwrap();
-        let kv2 = kvs.get(2).unwrap();
-
-        assert!(c.find(kv0.0.to_owned()).is_none());
-        assert!(c.find(kv1.0.to_owned()).is_none());
-        assert!(c.find(kv2.0.to_owned()).is_none());
-
-        c.insert(kv0.0.to_owned(), kv0.1);
-
-        assert_eq!(c.find(kv0.0.to_owned()), Some(&kv0.1));
-
-        c.insert(kv1.0.to_owned(), kv1.1);
-
-        assert_eq!(c.find(kv1.0.to_owned()), Some(&kv1.1));
-
-        c.insert(kv2.0.to_owned(), kv2.1);
-
-        assert_eq!(c.find(kv2.0.to_owned()), Some(&kv2.1));
-
-        // The cache is now full, and another insertion will make the first key/value be removed.
-
-        c.insert("blah".to_owned(), 42);
-
-        assert!(c.find(kv0.0.to_owned()).is_none());
-        assert!(c.find(kv1.0.to_owned()).is_some());
-        assert!(c.find(kv2.0.to_owned()).is_some());
-
-        c.insert("bleh".to_owned(), 42);
-        c.insert("bloh".to_owned(), 42);
-
-        assert!(c.find(kv1.0.to_owned()).is_none());
-        assert!(c.find(kv2.0.to_owned()).is_none());
-    }
-
-    #[test]
-    fn test_duplicate_insertions() {
-        let mut c = MemoCache::<String, i32, 2>::new();
-
-        let kv0 = ("John", 17);
-        let kv1 = ("Doe", 19);
-
-        assert!(c.find(kv0.0.to_owned()).is_none());
-        assert!(c.find(kv1.0.to_owned()).is_none());
-
-        c.insert(kv0.0.to_owned(), kv0.1);
-        c.insert(kv1.0.to_owned(), kv1.1);
-
-        assert!(c.find(kv0.0.to_owned()).is_some());
-        assert!(c.find(kv1.0.to_owned()).is_some());
-
-        // Inserting a duplicate key/value should be a no-op.
-
-        c.insert(kv0.0.to_owned(), kv0.1);
-
-        assert!(c.find(kv0.0.to_owned()).is_some());
-        assert!(c.find(kv1.0.to_owned()).is_some());
-
-        // Inserting a duplicate key with a new value should update the value.
-
-        assert_eq!(c.find(kv0.0.to_owned()), Some(&kv0.1));
-        assert_eq!(c.find(kv1.0.to_owned()), Some(&kv1.1));
-
-        c.insert(kv0.0.to_owned(), 42);
-
-        assert_eq!(c.find(kv0.0.to_owned()), Some(&42)); // Updated.
-        assert_eq!(c.find(kv1.0.to_owned()), Some(&kv1.1));
     }
 }
