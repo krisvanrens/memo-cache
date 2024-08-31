30363770d00d0016387baa75bcdfd69af74da2ef
Kris van Rens <krisvanrens@gmail.com>
2024-08-31 21:02:20 +0200

    CHG: Use a normal distribution for the example.

--- rs/memo-cache/examples/example.rs
+++ rs/memo-cache/examples/example.rs
@@ -1,15 +1,15 @@
 use memo_cache::MemoCache;
-use rand::Rng;
+use rand_distr::{Distribution, Normal};
 use std::{collections::HashMap, thread, time};
 
-fn some_expensive_calculation(_: u32) -> f32 {
+fn some_expensive_calculation(_: i32) -> f32 {
     thread::sleep(time::Duration::from_millis(50)); // ...zzzZZzz...
     std::f32::consts::PI
 }
 
 struct Process {
-    pub cache1: HashMap<u32, f32>,
-    pub cache2: MemoCache<u32, f32, 32>,
+    pub cache1: HashMap<i32, f32>,
+    pub cache2: MemoCache<i32, f32, 32>,
 }
 
 impl Process {
@@ -20,11 +20,11 @@ impl Process {
         }
     }
 
-    fn regular_method(&self, input: u32) -> f32 {
+    fn regular_method(&self, input: i32) -> f32 {
         some_expensive_calculation(input)
     }
 
-    fn memoized_method1(&mut self, input: u32) -> f32 {
+    fn memoized_method1(&mut self, input: i32) -> f32 {
         if let Some(value) = self.cache1.get(&input) {
             *value
         } else {
@@ -34,7 +34,7 @@ impl Process {
         }
     }
 
-    fn memoized_method2(&mut self, input: u32) -> f32 {
+    fn memoized_method2(&mut self, input: i32) -> f32 {
         if let Some(value) = self.cache2.get(&input) {
             *value
         } else {
@@ -52,16 +52,17 @@ fn main() {
     //   2. a method memoized using a hash map,
     //   3. a method memoized using a MemoCache cache.
     //
-    // Each of the methods are fed a series of random input numbers in a range
-    // 0..100 for which they (fake) "calculate" a result value. The memoized
-    // methods keep a local cache of result values for input values. The hash
-    // map will definitely perform best, but has no retention management -- its
-    // memory usage will grow with every new inserted input value. The method
-    // using the MemoCache cache will use a fixed-capacity cache and will per-
-    // form at best as good as the hash map cache version, and in the worst
-    // case as bad as the regular (non-memoized) method.
+    // Each of the methods are fed a series of random input numbers from a
+    // normal distribution for which they (fake) "calculate" a result value.
+    // The memoized methods keep a local cache of result values for input
+    // values. The hash map will definitely perform best, but has no retention
+    // management -- its memory usage will grow with every new inserted input
+    // value. The method using the MemoCache cache will use a fixed-capacity
+    // cache and will perform at best as good as the hash map cache version,
+    // and in the worst case as bad as the regular (non-memoized) method.
 
     let mut rng = rand::thread_rng();
+    let normal = Normal::new(0.0, 30.0).unwrap();
 
     let mut p = Process::new();
 
@@ -69,21 +70,21 @@ fn main() {
 
     let now = time::Instant::now();
     for _ in 0..100 {
-        p.regular_method(rng.gen_range(0..100));
+        p.regular_method(normal.sample(&mut rng) as i32);
     }
 
     let d_regular = now.elapsed();
 
     let now = time::Instant::now();
     for _ in 0..100 {
-        p.memoized_method1(rng.gen_range(0..100));
+        p.memoized_method1(normal.sample(&mut rng) as i32);
     }
 
     let d_memoized1 = now.elapsed();
 
     let now = time::Instant::now();
     for _ in 0..100 {
-        p.memoized_method2(rng.gen_range(0..100));
+        p.memoized_method2(normal.sample(&mut rng) as i32);
     }
 
     let d_memoized2 = now.elapsed();
